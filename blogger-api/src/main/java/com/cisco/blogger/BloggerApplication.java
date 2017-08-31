4f4a948d658349d28a3e5fdde7f49d0c7f3a8c1d
prpriyad <prpriyad@cisco.com>
2017-08-31 12:26:53 +0530

    Converted to jar to war packaging

--- blogger-api/src/main/java/com/cisco/blogger/BloggerApplication.java
+++ blogger-api/src/main/java/com/cisco/blogger/BloggerApplication.java
@@ -4,12 +4,19 @@ package com.cisco.blogger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BloggerApplication {
+public class BloggerApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BloggerApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BloggerApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BloggerApplication.class, args);
+    }
 
 }
